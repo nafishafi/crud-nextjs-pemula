147383a6b0fd0678adecdf1597edc4ceecc91466
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-05-15 11:40:14 +0700

    finished

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,7 @@
+import AddProduct from './addProduct';
+import DeleteProduct from './deleteProduct';
+import UpdateProduct from './updateProduct';
+
 type Product = {
     id: number;
     title: string;
@@ -5,17 +9,42 @@ type Product = {
 }
 
 async function getProducts() {
-    const res = await fetch('http://localhost:3001/products');
+    const res = await fetch('http://localhost:3001/products', {
+        cache: 'no-store',
+    });
     return res.json();
 }
 
 export default async function ProductList() {
     const products: Product[] = await getProducts();
   return (
-    <div>
-        {products.map((product, index) => (
-            <p key={product.id}>{product.title}</p>
-        ))}
+    <div className="py-10 px-10">
+        <div className="py-2">
+            <AddProduct/>
+        </div>
+        <table className="table w-full">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Product Name</th>
+                    <th>Price</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {products.map((product, index) => (
+                    <tr key={ product.id }>
+                        <td>{index + 1}</td>
+                        <td>{product.title}</td>
+                        <td>{product.price}</td>
+                        <td className='flex'>
+                            <UpdateProduct {...product}/>
+                            <DeleteProduct {...product}/>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     </div>
   )
 }
